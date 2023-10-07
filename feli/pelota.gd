872245f8c65870be29dc52aa4391fe7bee9bb732
Felipe Verri <47497965+PipeVerri@users.noreply.github.com>
2023-10-07 19:13:31 -0300

    pelotita anda

--- feli/pelota.gd
+++ feli/pelota.gd
@@ -2,7 +2,8 @@ extends RigidBody2D
 
 @export var tipo = 1
 
-const SPRING_CONSTANT := 100.0
+const SPRING_CONSTANT = 1000.0
 func _physics_process(delta):
+	self.rotation_degrees = 0
 	if Input.is_action_pressed("CLICK"):
-		apply_central_impulse(SPRING_CONSTANT * get_local_mouse_position() * delta)
+		self.linear_velocity = SPRING_CONSTANT * get_local_mouse_position() * delta
