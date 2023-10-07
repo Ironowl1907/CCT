114d30274d4f2b186b6c676fd5a4541cb29c4684
Felipe Verri <47497965+PipeVerri@users.noreply.github.com>
2023-10-07 19:29:32 -0300

    todo andando

--- feli/pelota.gd
+++ feli/pelota.gd
@@ -1,9 +1,19 @@
 extends RigidBody2D
 
 @export var tipo = 1
+var mouseOnTop = false
 
 const SPRING_CONSTANT = 1000.0
 func _physics_process(delta):
 	self.rotation_degrees = 0
-	if Input.is_action_pressed("CLICK"):
+	if Input.is_action_pressed("CLICK") and mouseOnTop:
 		self.linear_velocity = SPRING_CONSTANT * get_local_mouse_position() * delta
+
+
+func _on_detector_mouse_entered():
+	if not Input.is_action_pressed("CLICK"):
+		mouseOnTop = true
+
+func _on_detector_mouse_exited():
+	if not Input.is_action_pressed("CLICK"):
+		mouseOnTop = false;
--- feli/tacho.gd
+++ feli/tacho.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+var goodCounter = 0
+@export var goodType = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_area_2d_body_entered(body):
+	if (body.name != "StaticBody2D"):
+		if (body.tipo == goodType):
+			goodCounter += 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_area_2d_body_exited(body):
+	if (body.name != "StaticBody2D"):
+		if (body.tipo == goodType):
+			goodCounter -= 1
