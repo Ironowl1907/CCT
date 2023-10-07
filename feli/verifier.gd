d1ff5ae72b40863513ab48e31d0cc19702d18f5f
Felipe Verri <47497965+PipeVerri@users.noreply.github.com>
2023-10-07 19:46:21 -0300

    boom boom shackalacka

---
+++ feli/verifier.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var t1
+var t2
+var t3
+
+func _ready():
+	t1 = $tacho
+	t2 = $tacho2
+	t3 = $tacho3
+
+func _process(delta):
+	print (t1.goodCounter, t2.goodCounter, t3.goodCounter)
+	
+	if (t1.goodCounter >= 8 and t2.goodCounter >= 8 and t3.goodCounter >= 8):
+		print("SO!")
