de79231f9f0a043a1cb711f0f4607c39658eacbb
Felipe Verri <47497965+PipeVerri@users.noreply.github.com>
2023-10-08 18:20:52 -0300

    updated build

--- recursos/verifier.gd
+++ recursos/verifier.gd
@@ -10,7 +10,7 @@ func _ready():
 	t3 = $tacho3
 
 func _process(_delta):
-	if (t1.goodCounter >= 0 and t2.goodCounter >= 0 and t3.goodCounter >= 0):
+	if (t1.goodCounter >= 2 and t2.goodCounter >= 2 and t3.goodCounter >= 2):
 		$transitioner.play("fade")
 
 
