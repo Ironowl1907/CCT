0c80a78a13e09801497e70c5a9c308b932cfe099
Ironowl1907 <facuguialtana7@gmail.com>
2023-10-08 08:03:00 -0300

    Copia17

---
+++ cablecitos/Node.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+func AleaNum() -> int:
+	return randi_range(1, 4)
+
--- cablecitos/Scene/main.gd
+++ cablecitos/Scene/main.gd
@@ -4,7 +4,18 @@ var selected: bool = false
 
 
 func _draw() -> void:
-	draw_line($Cable.initalPos, $Cable.position, Color.RED, 10)
+	draw_line($Cable.initalPos, $Cable.position, Color.RED, 63)
+	draw_line($Cable2.initalPos, $Cable2.position, Color.BLUE, 63)
+	draw_line($Cable3.initalPos, $Cable3.position, Color.WHITE, 63)
+	draw_line($Cable4.initalPos, $Cable4.position, Color.GREEN, 63)
+
 
 func _process(_delta):
 	queue_redraw()
+
+
+
+
+#func AleaNum() -> int:
+#	return randi_range(1, 4)
+#
