e0640cfbb5da5cbf636ec95892bd134ab2570866
FishyBusinessYT <cristobalj.gonzalez@gmail.com>
2023-10-08 08:57:33 -0300

    .

--- Cris/Main.gd
+++ Cris/Main.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-var cut1 = preload()
+var cut1 = preload("res://cinematicas/c1.tscn")
 var cut2 = preload("res://lanzamiento/balance.tscn")
 var cut3 = preload("res://recursos/recursos.tscn")
 var cut4 = preload("res://cablecitos/Scene/main.tscn")
 var cut5 = preload("res://Cris/movie.tscn")
 
 func _ready() -> void:
-	add_child(cutscene1.instance())
+	add_child(cut2.instantiate())
