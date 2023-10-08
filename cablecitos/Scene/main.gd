faae019723f1ab73f10383b1b3e7c00a15e0c645
Ironowl1907 <facuguialtana7@gmail.com>
2023-10-08 00:36:40 -0300

    Copia11

---
+++ cablecitos/Scene/main.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var selected: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if selected:
+		var MousePosition = get_global_mouse_position()
+		$Cable.position = MousePosition
+
+
+
+func _on_cable_input_event(viewport, event, shape_idx):
+	if Input.is_action_pressed("CLICK"):
+		selected = true
--- cablecitos/papa.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var tipoDeNodo = -1;
-var nodoOriginal = Vector2()
