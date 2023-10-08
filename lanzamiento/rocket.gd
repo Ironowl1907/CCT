146dcef748dc13dcb746a6aebad358d9f9954198
Felipe Verri <47497965+PipeVerri@users.noreply.github.com>
2023-10-08 08:15:18 -0300

    balanceador funcionando

--- lanzamiento/rocket.gd
+++ lanzamiento/rocket.gd
@@ -10,6 +10,9 @@ func absolute(n):
 	else:
 		return n
 
+func _ready():
+	get_parent().get_node("Camera2D").shake(100, 100, 10)
+
 func _process(delta):
 	if rotation_degrees == 0:
 		rotationAccel = randi_range(1, -1) 
@@ -24,17 +27,16 @@ func _process(delta):
 		rotationAccel = 1 * s * delta * dupli
 	elif (absolute(rotation_degrees) < 20):
 		rotationAccel = 2 * s * delta * dupli
-	elif (absolute(rotation_degrees) < 30):
+	elif (absolute(rotation_degrees) < 40):
 		rotationAccel = 3 * s * delta * dupli
 	elif (absolute(rotation_degrees) < 60):
 		rotationAccel = 4 * s * delta * dupli
 	else:
-		print("BOOM")
 		rotationAccel = 0
 	
 	if Input.is_action_pressed("ui_left"):
-		rotationAccel -= 1
+		rotationAccel -= 60 * delta
 	elif Input.is_action_pressed("ui_right"):
-		rotationAccel += 1
+		rotationAccel += 60 * delta
 
 	rotation_degrees += rotationAccel
